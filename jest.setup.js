c0da392033d53978d79a6c5d0a80f815ca17c7a4
arelydtr <deyanitamr@gmail.com>
2024-11-28 17:10:03 -0600

    pruebas 1.0.7

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,7 @@
 import 'formdata-polyfill'; // Asegura que FormData esté definido
 
 // Mock para animaciones nativas (si estás usando Expo)
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+//jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 // Mock de Alert para evitar problemas en las pruebas
 jest.mock('react-native/Libraries/Alert/Alert', () => ({
